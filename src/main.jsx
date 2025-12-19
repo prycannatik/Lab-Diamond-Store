c8c20b1bcc4461ec0d5c3e79927f6b8a9447d36c
prycannatik <129606560+prycannatik@users.noreply.github.com>
2025-12-19 16:30:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LabDiamondStore from './LabDiamondStore'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LabDiamondStore />
+    <App />
   </React.StrictMode>,
 )
